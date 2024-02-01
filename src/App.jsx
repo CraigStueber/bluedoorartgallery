e21045e9a266dd254e81e7b0af41cdece90cd082
CraigStueber <craigstueber@gmail.com>
2024-01-31 19:43:04 -0500

    Clean Files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <p>Hi From Blue Door Art Gallery</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
